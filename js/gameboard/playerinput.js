e4167be62593cfcc505a7cc2ed01e0ae7d4dc1c5
Jesse McIntosh <jessemc98@hotmail.com>
2017-03-08 06:29:16 +0100

    changed keyboard input to be much more intuitive

--- js/gameboard/playerinput.js
+++ js/gameboard/playerinput.js
@@ -1,17 +1,36 @@
 export function setupMovement(gameBoard) {
+	const state = []
 	document.addEventListener('keydown', e => {
-		switch(e.key) {
-			case('w'):
-				return gameBoard.player.move('Up')
-			case('d'):
-				return gameBoard.player.move('Right')
-			case('s'):
-				return gameBoard.player.move('Down')
-			case('a'):
-				return gameBoard.player.move('Left')
+		if (e.key === 'w' || e.key === 'a' || e.key === 's' || e.key === 'd') {
+			if (state.indexOf(e.key) === -1) {
+				state.unshift(e.key)
+			}
 		}
+
+		movePlayer(state[0], gameBoard.player)
 	})
-	document.addEventListener('keyup', () => gameBoard.player.move())
+	document.addEventListener('keyup', e => {
+		const i = state.indexOf(e.key)
+		if (i > -1) {
+			state.splice(i, 1)
+		}
+		movePlayer(state[0], gameBoard.player)
+	})
 
 	requestAnimationFrame(gameBoard.update.bind(gameBoard))
+}
+
+function movePlayer(dir, player) {
+	switch(dir) {
+		case('w'):
+			return player.move('Up')
+		case('d'):
+			return player.move('Right')
+		case('s'):
+			return player.move('Down')
+		case('a'):
+			return player.move('Left')
+		default:
+			return player.move()
+	}
 }
